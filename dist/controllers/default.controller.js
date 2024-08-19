de25c529dc8213f8349ae862b098c9a7738950ce
yhiriend <yirien45@gmail.com>
2024-08-19 11:43:37 -0500

    remove dist dir

--- dist/controllers/default.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getDefault = void 0;
-const getDefault = (req, res) => {
-    res.json({
-        msg: 'API funcionando'
-    });
-};
-exports.getDefault = getDefault;
--- dist/utils/time-converter.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const convertMinutesInMillis = (minutes) => {
-    return minutes * 60 * 1000;
-};
-exports.default = convertMinutesInMillis;
